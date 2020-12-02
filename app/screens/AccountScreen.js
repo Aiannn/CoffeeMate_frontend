e12f699713f8d94b2644eb1506db6a11ba64b7d4
Aiannn <soktoevayan@gmail.com>
2020-12-01 21:52:36 -0500

    01.12.2020 upd

--- app/screens/AccountScreen.js
+++ app/screens/AccountScreen.js
@@ -1,16 +1,31 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, Image } from 'react-native';
 
 function AccountScreen(props) {
     return (
         <SafeAreaView style={styles.container}>
-            <Text>Your Account Information</Text>
+            <View style={styles.card}>
+                <Image source={require('../../assets/coffee-shop-1.jpg')} style={styles.image} />
+                <Text>Name</Text>
+                <Text>Age</Text>
+                <Text>Sex</Text>
+            </View>
         </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
-    container: {}
+    container: {},
+    card: {
+        borderWidth: 5,
+        backgroundColor: 'brown',
+        borderColor: 'green',
+        alignItems: 'center'
+    },
+    image: {
+        width: 300,
+        height: 300
+    }
 });
 
 export default AccountScreen;
