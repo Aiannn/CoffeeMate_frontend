83d1cbfb59a6e867393e668931e0b540dd68536a
Aiannn <soktoevayan@gmail.com>
2020-12-06 21:21:12 -0500

    06.12.2020 upd2

--- app/screens/CreateMeetingScreen.js
+++ app/screens/CreateMeetingScreen.js
@@ -1,19 +1,29 @@
 import React from 'react';
+import { useState } from 'react'
 import { SafeAreaView, StyleSheet, Text, Button, TextInput, View } from 'react-native';
 
+const handlePress = () => {
+    alert(CoffeeShop)
+}
+//need to refactor tomorrow
 function CreateMeetingScreen(props) {
+
+    const [CoffeeShop, setCoffeeShop] = useState()
+    const [Time, setTime] = useState()
+    const [Duration, setDuration] = useState()
+
     return (
         <SafeAreaView style={styles.container}>
             <Text>Create new Meeting</Text>
             <View style={styles.form}>
                 <Text>Coffeeshop:</Text>
-                <TextInput title='Coffeeshop' placeholder='enter coffeeshop' />
+                <TextInput value={CoffeeShop} title='Coffeeshop' placeholder='enter coffeeshop' onChangeText={setCoffeeShop()} />
                 <Text>Time:</Text>
                 <TextInput title='Time' placeholder='enter time' />
                 <Text>duration:</Text>
                 <TextInput title='Duration' placeholder='enter duration' />
             </View>
-            <Button title='Create' />
+            <Button title='Create' onPress={handlePress} />
         </SafeAreaView>
     );
 }
