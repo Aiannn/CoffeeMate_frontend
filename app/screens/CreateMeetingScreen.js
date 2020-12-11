7abed34a426dd593ffc0347a3f6c890eaecc975d
Aiannn <soktoevayan@gmail.com>
2020-12-10 23:44:27 -0500

    10.12.2020 upd

--- app/screens/CreateMeetingScreen.js
+++ app/screens/CreateMeetingScreen.js
@@ -1,31 +1,41 @@
 import React from 'react';
-import { useState } from 'react'
 import { SafeAreaView, StyleSheet, Text, Button, TextInput, View } from 'react-native';
 
-const handlePress = () => {
-    alert(CoffeeShop)
-}
-//need to refactor tomorrow
-function CreateMeetingScreen(props) {
+/
+class CreateMeetingScreen extends React.Component() {
 
-    const [CoffeeShop, setCoffeeShop] = useState()
-    const [Time, setTime] = useState()
-    const [Duration, setDuration] = useState()
+    state = {
+        Coffeeshop: '',
+        Time: '',
+        Duration: 0
+    }
 
-    return (
-        <SafeAreaView style={styles.container}>
-            <Text>Create new Meeting</Text>
-            <View style={styles.form}>
-                <Text>Coffeeshop:</Text>
-                <TextInput value={CoffeeShop} title='Coffeeshop' placeholder='enter coffeeshop' onChangeText={setCoffeeShop()} />
-                <Text>Time:</Text>
-                <TextInput title='Time' placeholder='enter time' />
-                <Text>duration:</Text>
-                <TextInput title='Duration' placeholder='enter duration' />
-            </View>
-            <Button title='Create' onPress={handlePress} />
-        </SafeAreaView>
-    );
+    handlePress = () => {
+        alert.alert(this.state.Coffeeshop, this.state.Time, this.state.Duration)
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.title]: e.target.value
+        })
+    }
+
+    render() {
+        return (
+            <SafeAreaView style={styles.container}>
+                <Text>Create new Meeting</Text>
+                <View style={styles.form}>
+                    <Text>Coffeeshop:</Text>
+                    <TextInput value={this.state.Coffeeshop} title='Coffeeshop' placeholder='enter coffeeshop' onChangeText={this.handleChange} />
+                    <Text>Time:</Text>
+                    <TextInput value={this.state.Time} title='Time' placeholder='enter time' onChangeText={this.handleChange} />
+                    <Text>duration:</Text>
+                    <TextInput value={this.state.Duration} title='Duration' placeholder='enter duration' onChangeText={this.handleChange} />
+                </View>
+                <Button title='Create' onPress={handlePress} />
+            </SafeAreaView>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
